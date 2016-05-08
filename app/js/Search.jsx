23f58769549d1e51be4dea7a42331bfb75914fd5
Keenan Staffieri <kstaffieri@gmail.com>
2016-05-07 18:53:25 -0700

    going through complete react tutorial

--- app/js/Search.jsx
+++ app/js/Search.jsx
@@ -2,14 +2,36 @@ const React = require('react')
 const ShowCard = require('./ShowCard')
 const data = require('./data')
 
-const Search = () => (
-  <div className='container'>
-    <div className='shows'>
-      {data.shows.map((show, index) => (
-        <ShowCard {...show} />
-      ))}
-    </div>
-  </div>
-)
+class Search extends React.Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      searchTerm: 'this is the default searchTerm'
+    }
+
+    this.handleSearchTermChange = this.handleSearchTermChange.bind(this)
+  }
+  handleSearchTermChange (event) {
+    this.setState({ searchTerm: event.target.value })
+  }
+  render () {
+    return (
+      <div className='container'>
+        <header className='header'>
+          <h1 className='brand'>{this.state.searchTerm}</h1>
+          <input type='text' className='search-input' placeholder='Search' value={this.state.searchTerm} onChange={this.handleSearchTermChange} />
+        </header>
+        <div className='shows'>
+          {data.shows
+            .filter((show) => `${show.title} ${show.description}`.toUpperCase().indexOf(this.state.searchTerm.toUpperCase()) >= 0)
+            .map((show) => (
+              <ShowCard {...show} />
+          ))}
+        </div>
+      </div>
+    )
+  }
+}
 
 module.exports = Search
