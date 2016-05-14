021da4671f61de03d16c70a42b22e170e9893929
Keenan Staffieri <kstaffieri@gmail.com>
2016-05-14 14:19:42 -0700

    added koa-router

--- api/index.js
+++ api/index.js
@@ -1,11 +1,14 @@
 const Koa = require('koa')
 const app = new Koa()
+var router = require('koa-router')()
+
+const API_PORT = 9000;
 
 app.name = 'Silvvr Application'
 app.env = 'dev'
 
 // x-response-time
-app.use(async function (ctx, next) {
+app.use(async (ctx, next) => {
   const start = new Date()
   await next()
   const ms = new Date() - start
@@ -13,16 +16,30 @@ app.use(async function (ctx, next) {
 })
 
 // logger
-app.use(async function (ctx, next) {
+app.use(async (ctx, next) => {
   const start = new Date()
   await next()
   const ms = new Date() - start
   console.log(`${ctx.method} ${ctx.url} - ${ms}`)
-});
-
-// response
-app.use(ctx => {
-  ctx.body = 'Hello World'
 })
 
-app.listen(3000)
+// response
+// app.use(ctx => {
+//   ctx.body = 'Hello World'
+// })
+
+router.get('/', (ctx, next) => {
+  ctx.body = 'Index Page'
+})
+
+router.get('/test', (ctx, next) => {
+  ctx.body = 'Test Page'
+})
+
+app
+  .use(router.routes())
+  .use(router.allowedMethods())
+
+app.listen(API_PORT, function () {
+  console.log('API server listening on port ' + API_PORT)
+})
