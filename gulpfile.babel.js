077db4b4034031b6321cc710884f065a8e0725cd
Keenan Staffieri <kstaffieri@gmail.com>
2016-06-11 10:04:58 -0700

    remove uneeded npm modules

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -2,26 +2,14 @@
   - - - - - -
   S I L V V R
   - - - - - -
-  Full-stack NodeJS API & frontend Gulp build system.
+  gulpfile.babel
+  Frontend Gulp build system and boilerplate.
   @author Keenan Staffieri <http://keenanstaffieri.com>
   Twitter: @keenode
 */
 
-// Require babel for transpiling
-require('babel-core/register')()
-require('babel-polyfill')
-
 // Application root directory
 global.APP_ROOT = __dirname
 
-// Require API server
-const api = require('./api')
-
-// Require Gulp
-const gulp = require('gulp')
-
-// Require build system
-const buildsys = require('./buildsys')
-
-// Start Gulp default task
-gulp.start('default')
+// Import build system ( ./buildsys/index.js ) is automatically loaded first.
+import buildsys from './buildsys'
